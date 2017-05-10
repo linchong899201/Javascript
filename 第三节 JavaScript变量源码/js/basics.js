f53747798bf24d98c856cf35d2fdc3a2fc84be1e
Peter <linchong899201@hotmail.com>
2017-05-10 22:54:39 +0800

    JavaScript基础入门
    JavaScript基础入门第三节 JavaScript变量源码

---
+++ 第三节 JavaScript变量源码/js/basics.js
@@ -0,0 +1,14 @@
+//console.log('点点滴滴');
+
+/*
+console.log('adfsads');
+alert('adsfadsfads');
+*/
+
+//console.log('>3');
+//alert('警告你要动手操作！');
+var thedMeaningOfLearning = 30,foo=bac,bac,addd;
+
+foo=bac;	
+
+
