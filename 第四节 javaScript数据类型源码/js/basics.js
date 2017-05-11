7bfb1851799316415c2dd9897b92a53dea16be4b
Peter <linchong899201@hotmail.com>
2017-05-11 23:09:30 +0800

    第四节 javaScript数据类型源码
    第四节 javaScript数据类型源码课堂源码

---
+++ 第四节 javaScript数据类型源码/js/basics.js
@@ -0,0 +1,33 @@
+//console.log('点点滴滴');
+
+/*
+console.log('adfsads');
+alert('adsfadsfads');
+*/
+
+//console.log('>3');
+//alert('警告你要动手操作！');
+var thedMeaningOfLearning = 30,foo=bac,bac,addd;
+
+foo=bac;	
+
+var number=1;
+
+var string = 'hello';
+
+var quotes = '"你好",他说';
+
+var escaped = '"he doesn\'t" she said';
+
+var array = [5,'abc',true];
+
+var object = {
+	myNumber:5,
+	myString:'hello!'
+};
+console.log(object.myNumber);
+
+console.log(object.myString);
+
+
+
