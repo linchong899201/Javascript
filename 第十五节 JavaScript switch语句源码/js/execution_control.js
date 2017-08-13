e787762d97c447e7f60f60972f4cea7244b12543
Peter <linchong899201@hotmail.com>
2017-08-13 15:26:00 +0800

    js课堂源码第十四到第十五节
    js课堂源码第十四到第十五节

---
+++ 第十五节 JavaScript switch语句源码/js/execution_control.js
@@ -0,0 +1,75 @@
+(function(){
+	'use strict';
+	if (1+1 === 3){
+		console.log('这个表达式是true');
+	}else{
+		console.log('这个表达式是false')
+	}
+
+	if(true === false){
+		console.log('我们不能看到');
+	}else{
+		console.log('我们总是看到');
+	}
+
+	var color = 'red';
+	if(color === 'blue'){
+		console.log('是蓝色');
+	}else if(color === 'green'){
+		console.log('是绿色');
+	}else{
+		console.log('是红色');
+	}
+
+	if(1){
+		console.log('我们能够看到它');
+	}
+
+	var array = ['item1','abc'];
+	if (array.length){
+		console.log(array[1])
+	}
+
+	(true===false)?console.log('是这个'):console.log('不是这个');
+
+	(true)?console.log('短头发'):console.log('长头发');
+}());
+
+var switcher = 'two';
+
+switch(switcher){
+	case 'one':
+		console.log('one');
+		break;
+
+	case 'two':
+		console.log('two');
+	break;
+}
+
+//没有break的情况
+var switcher = 'one';
+switch(switcher){
+	case 'one':
+		console.log('one');
+	case 'two':
+		console.log('two');
+	break;
+}
+
+
+//default的情况
+var switcher = 'three';
+switch(switcher){
+	case 'one':
+		console.log('one');
+		break;
+
+	case 'two':
+		console.log('two');
+	break;
+
+	default:
+		console.log(switcher);
+}
+
